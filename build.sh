ebc346f736a12491a40be4cbd5831a665d613c78
Frank Purcell <fxpurcell@gmail.com>
2017-04-18 17:46:16 -0700

    more initial work

---
+++ build.sh
@@ -0,0 +1,18 @@
+if [ ! -d "lib" ];
+then
+    mkdir lib
+fi
+
+if [ ! -f "lib/swagger-codegen-cli.jar" ];
+then
+    wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.2/swagger-codegen-cli-2.2.2.jar -O lib/swagger-codegen-cli.jar
+fi
+
+echo "##############################################"
+echo "# to run the code generator                  #"
+echo "#                                            #"
+echo "# java -jar lib/swagger-codegen-cli.jar help #"
+echo "#                                            #"
+echo "##############################################"
+
+java -jar lib/swagger-codegen-cli.jar help
