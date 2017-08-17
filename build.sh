2e77195680070e2ad81deb9b26beb09ebea1f174
Frank Purcell <fxpurcell@gmail.com>
2017-08-17 15:38:39 -0700

    try swag 3.0.0

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ fi
 if [ ! -f "lib/swagger-codegen-cli.jar" ];
 then
     ver=2.2.3
-    wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$ver/swagger-codegen-cli-$ver.jar -O lib/swagger-codegen-cli.jar
+    url=http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$ver/swagger-codegen-cli-$ver.jar
+    #url=https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/3.0.0-SNAPSHOT/swagger-codegen-cli-3.0.0-20170727.135949-1.jar
+    wget $url -O lib/swagger-codegen-cli.jar
 fi
 
 echo "##############################################"
@@ -18,10 +20,15 @@ echo "##############################################"
 
 echo "https://github.com/swagger-api/swagger-codegen?ref=producthunt#overview"
 
+swagger_input=swagger.yaml
+#swagger_input=swagger/uber.yaml
+swagger_input=swagger/TransitApi.json
+
+
 langs=(java javascript python python-flask html)
 for l in "${langs[@]}"
 do
    mkdir -p bindings/$l
-   echo java -jar lib/swagger-codegen-cli.jar generate -i swagger.yaml -l $l -o bindings/$l
-   java -jar lib/swagger-codegen-cli.jar generate -i swagger.yaml -l $l -o bindings/$l > codegen.log
+   echo java -jar lib/swagger-codegen-cli.jar generate -i ${swagger_input} -l $l -o bindings/$l
+   java -jar lib/swagger-codegen-cli.jar generate -i ${swagger_input} -l $l -o bindings/$l > codegen.log
 done
