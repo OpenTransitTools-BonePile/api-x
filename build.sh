6f0295287c8c3f1c35fe0a5b604030992c5c7b19
Frank Purcell <fxpurcell@gmail.com>
2017-08-17 18:57:36 -0700

    fixed with a ./ relative path def

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ fi
 
 if [ ! -f "lib/swagger-codegen-cli.jar" ];
 then
-    ver=2.2.3
+    ver=2.3.0
     url=http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$ver/swagger-codegen-cli-$ver.jar
+    url=https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-2.3.0-20170815.070713-92.jar
     #url=https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/3.0.0-SNAPSHOT/swagger-codegen-cli-3.0.0-20170727.135949-1.jar
     wget $url -O lib/swagger-codegen-cli.jar
 fi
@@ -20,10 +21,7 @@ echo "##############################################"
 
 echo "https://github.com/swagger-api/swagger-codegen?ref=producthunt#overview"
 
-swagger_input=swagger.yaml
-#swagger_input=swagger/uber.yaml
 swagger_input=swagger/TransitApi.json
-
 
 langs=(java javascript python python-flask html)
 for l in "${langs[@]}"
